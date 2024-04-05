8641be87bb3e924dd7a507c2e7a0e6c8a573a965
Ethan19922 <158313921+Ethan19922@users.noreply.github.com>
2024-04-05 10:42:59 -0500

    changes

--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -2,11 +2,35 @@ package studentdriver;
 
 public class UGStudent extends StudentFees{
     private double scholarshipAmount;
-    private int courseEnrolled;
+    private int coursesEnrolled;
     private boolean hasScholarship;
     private double ADDITIONAL_FEE = 820.70;
-    public UGStudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int courseEnrolled) {
+    
+    public UGStudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, double scholarshipAmount, int coursesEnrolled) {
         super(studentName, studentID, isEnrolled);
+        this.hasScholarship = hasScholarship;
+        this.scholarshipAmount = scholarshipAmount;
+        this.coursesEnrolled = coursesEnrolled;
     }
     
+    public boolean isHasScholarship(){
+        return this.hasScholarship;
+    }
+    
+    public double getScholarshipAmount(){
+        return this.scholarshipAmount;
+    }
+    
+    public int getCoursesEnrolled(){
+        return this.coursesEnrolled;
+    }
+    
+    public double getPayableAmountt(){
+        return (super.getPayableAmount() * 3) + this.ADDITIONAL_FEE;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + "\nScholarship: " + this.hasScholarship + "\nScholarship amount: " + this.scholarshipAmount + "\nCourses enrolled: " + this.coursesEnrolled + "Payable amount: " + getPayableAmountt();
+    }
 }
