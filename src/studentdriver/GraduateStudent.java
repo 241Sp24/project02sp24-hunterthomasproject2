440930fafb1c27e95131fa30e48e779b23c46a0d
Mariah Thomas <142917025+MariahThomas03@users.noreply.github.com>
2024-04-08 18:16:24 -0500

    graduate assistant
    finishing this section up

--- src/studentdriver/GraduateStudent.java
+++ src/studentdriver/GraduateStudent.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package studentdriver;
 
 /**
@@ -9,5 +6,13 @@ package studentdriver;
  * @author s558312
  */
 public class GraduateStudent extends StudentFees{
+    private int coursesEnrolled;
+    private boolean isGraduateAssitant;
+    private String GraduateAssistantType;
+    private double ADDITIONAL_FEES = 645.45;
+    
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssitant, String GraduateAssitantType, int coursesEnrolled){
+     super(studentName, studentID, isEnrolled, isGraduateAssitant, coursesEnrolled);  
+    }
     
 }
